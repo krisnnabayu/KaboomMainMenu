491562ae4f2ef9b6f9a5a111e3bff9cf2746edc3
krisnnabayu <krisna.bayu98@gmail.com>
2020-12-31 15:37:08 +0700

    Initial commit

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame ()
+    {
+        SceneManager.LoadScene("");
+
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("gamenya keluar kalo udah di build bos");
+        Application.Quit();
+
+    }
+}
